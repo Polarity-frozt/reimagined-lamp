9822009d7440c90cef1a45c8e745de850ad4e2a3
Polarity-frozt <shanesly2027@gmail.com>
2026-09-16 08:59:40 +1000

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
-  },
+self.__uv$config = {
+  prefix: "/a/",
+  bare: "/ca/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/assets/mathematics/handler.js?v=9-30-2024",
+  bundle: "/assets/mathematics/bundle.js?v=9-30-2024",
+  config: "/assets/mathematics/config.js?v=9-30-2024",
+  sw: "/assets/mathematics/sw.js?v=9-30-2024",
 };
-
-export default config;
